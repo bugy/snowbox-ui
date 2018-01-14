b4bb236d2a3920fd5c2b698f618445ace5e9d1af
bugy <buggygm@gmail.com>
2018-01-14 22:13:21 +0100

    fixed broken Gray library link

--- js/phaser-helper.js
+++ js/phaser-helper.js
@@ -2,7 +2,7 @@ var disabledButtonGray;
 
 function preloadHelper() {
     game.load.script('disabledButtonGray',
-        'https://cdn.rawgit.com/photonstorm/phaser/master/v2/filters/Gray.js');
+        'http://cdn.rawgit.com/photonstorm/phaser-ce/release/v2.9.3/filters/Gray.js');
 }
 
 function setButtonEnabled(button, enabled) {
