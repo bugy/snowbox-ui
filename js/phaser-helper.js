540c4b1dc5a5a8e27db775048964e438f08473e9
Iaroslav Shepilov <buggygm@gmail.com>
2018-02-22 09:42:17 +0100

    fixed the link to Gray filter

--- js/phaser-helper.js
+++ js/phaser-helper.js
@@ -2,7 +2,7 @@ var disabledButtonGray;
 
 function preloadHelper() {
     game.load.script('disabledButtonGray',
-        'http://cdn.rawgit.com/photonstorm/phaser-ce/release/v2.9.3/filters/Gray.js');
+        'https://cdn.rawgit.com/photonstorm/phaser-ce/release/v2.9.3/filters/Gray.js');
 }
 
 function setButtonEnabled(button, enabled) {
